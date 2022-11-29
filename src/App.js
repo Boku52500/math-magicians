b6a9e090c83db889a720eb47a3e25547aaca694f
Giorgi Bokuchava <g.bokuchava22@gmail.com>
2022-11-29 10:22:14 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,36 +14,36 @@ class App extends Component {
 
   render() {
     return (
-      <div className='app'>
-        <div className='calc-wrapper'>
+      <div className="app">
+        <div className="calc-wrapper">
           {/* eslint-disable-next-line react/destructuring-assignment */}
           <Input input={this.state.input} />
-          <div className='row'>
+          <div className="row">
             <Button>AC</Button>
             <Button>+/-</Button>
             <Button>%</Button>
             <Button>÷</Button>
           </div>
-          <div className='row'>
+          <div className="row">
             <Button>7</Button>
             <Button>8</Button>
             <Button>9</Button>
             <Button>X</Button>
           </div>
-          <div className='row'>
+          <div className="row">
             <Button>4</Button>
             <Button>5</Button>
             <Button>6</Button>
             <Button>-</Button>
           </div>
-          <div className='row'>
+          <div className="row">
             <Button>1</Button>
             <Button>2</Button>
             <Button>3</Button>
             <Button>+</Button>
           </div>
-          <div className='row'>
-            <div className='zero'>
+          <div className="row">
+            <div className="zero">
               <Button>0</Button>
             </div>
             <Button>.</Button>
