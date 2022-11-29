270d74f20efa56afbeafa0b67e0b512be3712e28
Giorgi Bokuchava <g.bokuchava22@gmail.com>
2022-11-29 08:56:51 -0800

    Add Calculator UI

---
+++ src/components/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Button.css';
+
+const isOperator = (val) => {
+  return (
+    !isNaN(val) || val === '.' || val === 'AC' || val === '+/-' || val === '%'
+  );
+};
+
+export const Button = (props) => (
+  <div
+    className={`button-wrapper
+ ${isOperator(props.children) ? null : 'operator'}`}
+  >
+    {props.children}
+  </div>
+);
