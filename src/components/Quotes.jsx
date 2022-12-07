726f22b2a9366fc92091965c5d346457cd33cb2a
Giorgi Bokuchava <g.bokuchava22@gmail.com>
2022-12-06 17:50:48 -0800

    Created Full Website

---
+++ src/components/Quotes.jsx
@@ -0,0 +1,12 @@
+import './Quotes.css';
+
+const Quote = () => (
+  <div className="container">
+    <p className="quote">
+      Pure mathematics is, in its way, the poetry of logical ideas.
+      — Albert Einstein, German theoretical physicist
+    </p>
+  </div>
+);
+
+export default Quote;
